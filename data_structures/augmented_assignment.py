c7aed91bfc0cc144fe67b6f480fa765a5af169c0
Matheus Raposo <matheus_raposo@hotmail.com>
2022-02-12 17:43:29 -0300

    add augmented assignment

---
+++ data_structures/augmented_assignment.py
@@ -0,0 +1,30 @@
+# <<< Mutable vs. Immutable Objects >>>
+
+# Mutable: the augmented assignment, the __iadd__ is implemented and the
+# change occurs in place
+l = [1, 2, 3]
+print(id(l))  # 4697121408
+l *= 2
+print(l)
+print(id(l))  # 4697121408
+
+# Immutable: the change creates a new object
+t = (1, 2, 3)
+print(id(t))  # 4694154240
+t *= 2
+print(id(t))  # 4696957280
+
+# Bottomline === Repeated concatenation of immutable sequences is inefficient.
+
+
+# <<< Tuple Riddle >>>
+import dis
+
+t = (1, 2, [30, 40])
+t[2] += [50, 60] # Throws TypeError: 'tuple' object does not support item assignment
+# Get bytecode for expression
+dis.dis('s[a] += b')
+# Bottomline === Putting mutable items in tuples is not a good idea.
+
+
+
